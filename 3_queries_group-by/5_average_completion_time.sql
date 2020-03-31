d7b3686bb33b20bdd91ac3405af7d790b894e0ca
BilliMorgan <shimko20@gmail.com>
2020-03-31 02:35:49 +0000

    add having and group

---
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -0,0 +1,7 @@
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions on students.id = student_id
+
+WHERE students.end_date IS NULL
+GROUP BY student
+ORDER BY avg(assignment_submissions.duration) DESC;
