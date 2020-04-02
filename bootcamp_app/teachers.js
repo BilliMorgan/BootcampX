ec4c2797ba5bee01fa25cb7d6e6cb073e46d5387
BilliMorgan <shimko20@gmail.com>
2020-04-02 02:21:44 +0000

    parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,20 +6,25 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-let cohortName = process.argv[2]
-//console.log(cohortName)
 
-pool.query(`
-SELECT distinct teachers.name as teacher, cohorts.name as cohort
-FROM assistance_requests
-JOIN teachers on teachers.id = teacher_id
-JOIN students on students.id = student_id
-JOIN cohorts on cohorts.id = cohort_id
-WHERE cohorts.name = '${cohortName}'
-ORDER BY teachers.name;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+let cohortName = process.argv[2];
+console.log(cohortName);
+const values = [cohortName];
+
+const queryString = `
+  SELECT distinct teachers.name as teacher, cohorts.name as cohort
+  FROM assistance_requests
+  JOIN teachers on teachers.id = teacher_id
+  JOIN students on students.id = student_id
+  JOIN cohorts on cohorts.id = cohort_id
+  WHERE cohorts.name = $1
+  ORDER BY teachers.name;
+`;
+
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
+    })
+  }).catch(err => console.error('query error', err.stack));
